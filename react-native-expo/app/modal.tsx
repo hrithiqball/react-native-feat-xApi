3dd10955a916d245e9708c0b3bb2fd3efa0621b7
Harith Iqbal <harithmu@gmail.com>
2023-12-13 17:50:11 +0800

    chore: nativewind, linting and formatting
    explore husky

--- react-native-expo/app/modal.tsx
+++ react-native-expo/app/modal.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet } from "react-native";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
 
 export default function ModalScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-bold text-xl">Modal</Text>
       <View
+        className="my-8 w-80%"
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
@@ -17,7 +18,7 @@ export default function ModalScreen() {
       <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 }
@@ -25,16 +26,16 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%',
   },
 });
